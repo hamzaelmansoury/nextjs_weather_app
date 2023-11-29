414ff098ba9d13df38a7094dc8b7e33ebd054908
hamzaelmansoury <131284674+hamzaelmansoury@users.noreply.github.com>
2023-11-29 19:13:10 +0000

    Update opacity image to 40

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
           <link rel='icon' href='/favicon.ico' />
         </Head>
         {/* Overlay */}
-        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/60 z-[1]' />
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]' />
         {/* Background image */}
         <Image
           src='https://images.unsplash.com/photo-1596627008830-41d373a44a96?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
